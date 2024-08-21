b28ea85d70f427234a7cdfe7914873ac94e5d919
Felipe Martelo <54710263+Fleyvera@users.noreply.github.com>
2024-08-21 08:31:54 -0300

    Wip

---
+++ src/components/buttonCustom/index.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacity, View, Text } from "react-native"
+
+export const ButtonCustom = () => {
+    return(
+        <View>
+            <TouchableOpacity>
+                 <Text>
+                    Cadastrar
+                 </Text>
+            </TouchableOpacity>  
+        </View>
+    )
+}
