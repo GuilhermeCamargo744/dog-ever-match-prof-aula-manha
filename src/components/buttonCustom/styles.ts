d4083aa2db1a15552b356e4cb689fe6dbf9619b3
Felipe Martelo <54710263+Fleyvera@users.noreply.github.com>
2024-08-21 08:32:13 -0300

    Wip

---
+++ src/components/buttonCustom/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+
+export const style = StyleSheet.create({
+    button: {
+        backgroundColor: "#FFF",
+        height: 53,
+        width: "100%",
+        borderRadius: 8,
+        elevation: 5,
+        shadowColor: "#000",
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+    }
+})
