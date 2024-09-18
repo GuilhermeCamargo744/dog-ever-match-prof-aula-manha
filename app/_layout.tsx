1e5cceb6c1eed3d7427fd1fcf2d31634e4a70f3c
Felipe Martelo <54710263+Fleyvera@users.noreply.github.com>
2024-09-18 07:17:01 -0300

    Wip aula-6

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(app)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
